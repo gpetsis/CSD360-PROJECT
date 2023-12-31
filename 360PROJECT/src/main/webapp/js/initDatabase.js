504752e85be86de5041829dca1ce032e94057853
Giannis Petsis <giannispetsis5@gmail.com>
2023-12-31 14:23:15 +0200

    [Giannis] Add form for new customer

--- 360PROJECT/src/main/webapp/js/initDatabase.js
+++ 360PROJECT/src/main/webapp/js/initDatabase.js
@@ -1,5 +1,8 @@
 function initDatabase() {
     var xhr = new XMLHttpRequest();
+    
+    
+    
     xhr.onload = function () {};
     xhr.open('GET', 'Init');
     xhr.setRequestHeader("Content-type", "application/json");

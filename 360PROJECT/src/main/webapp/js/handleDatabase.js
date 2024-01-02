7b4436fcea415712d490f3bbf7591432ebce26bf
Nikos Lasithiotakis <csd4924@csd.uoc.gr>
2024-01-02 13:04:44 +0200

    [Nikos] Rename file

--- 360PROJECT/src/main/webapp/js/handleDatabase.js
+++ 360PROJECT/src/main/webapp/js/handleDatabase.js
@@ -1,8 +1,5 @@
 function initDatabase() {
     var xhr = new XMLHttpRequest();
-    
-    
-    
     xhr.onload = function () {};
     xhr.open('GET', 'Init');
     xhr.setRequestHeader("Content-type", "application/json");
